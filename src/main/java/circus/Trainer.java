b541e112253ca88a136416e46428a4ac51085731
geezzzyyy <zhuoyange@gmail.com>
2021-02-08 22:38:20 +0800

    All changes

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -1,3 +1,9 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Bird;
+import circus.animal.Duck;
+
 public class Trainer {
     public static void main(String[] args) {
         Duck d = new Duck();
@@ -5,23 +11,25 @@ public class Trainer {
 
         Bird b = (Bird)d;  // upcasting
         getToSpeak(b);
-
+// these upcasting are redundant
         Animal a = (Animal)b; // upcasting
         getToSpeak(a);
 
         Duck d2 = (Duck) a; // downcasting
         train(new Duck());
         // train(new Parrot());
-        Animal a2 = new Animal();
-        Bird b2 = new Bird();
+        //Animal a2 = new Animal();
+        //Bird b2 = new Bird();
     }
-
+    //substitutability
     private static void getToSpeak(Animal animal) {
         System.out.println(animal.speak());
-    }
+    }// at runtime, look at the object not ref
 
     private static void train(Bird bird) {
-        Duck d = (Duck) bird;
-        d.swim();
+        if (bird instanceof Duck) {//search Action: surrandwith
+            Duck d = (Duck) bird;
+            d.swim();
+        }
     }
 }
